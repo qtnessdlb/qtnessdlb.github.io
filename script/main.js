8b1990d093d43fb8bc14373bbcb9a24e7e98ea96
izumiinyaa <adimasbbayuarto@email.com>
2023-12-31 21:54:48 +0700

    fixing bug, and adding some features

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
-// toggle menu
-$('.menu-toggle').on('click', function(){
-  $('body').toggleClass('open');
-});
+// // toggle menu
+// $('.menu-toggle').on('click', function(){
+//   $('body').toggleClass('open');
+// });
 
 $(".menu-toggle").click(function() {
   $('.menu-btn-icon').toggleClass("activated");
@@ -25,3 +25,33 @@ $(document).ready(function(e) {
 	});
 });
 
+
+
+$(document).ready(function(){
+
+	
+	$('.menu-toggle').click(function(){
+		
+		var $this = $( this );
+		
+		if ($this.hasClass('is-active')){
+			$('.fsmenu, .logo').removeClass('is-active');
+			$('.fsmenu, .logo').addClass('close-menu');
+		} else{
+			$('.fsmenu, .logo').removeClass('close-menu');
+			$('.fsmenu, .logo').addClass('is-active');
+		};
+		$this.toggleClass('is-active');
+	});
+	
+	$( ".fsmenu--list-element" ).hover(
+		function() {
+			$( this ).addClass('open');
+			$( this ).removeClass('is-closing');
+		}, function() {
+			$( this ).removeClass('open');
+			$( this ).addClass('is-closing');
+		}
+	);
+				
+});
