c6000e6028af7f7edaff8ddb14489d6adc3737b0
Izumiinyaa <adimasbbayuarto@gmail.com>
2024-01-04 16:27:58 +0700

    update bug fix

--- script/leaderboard.js
+++ script/leaderboard.js
@@ -19,11 +19,5 @@ var leaderboardData = [
         username: 'izumiinyaa', 
         instagramUsername: 'qtnessdlb', 
         description: 'Startend - Hololive'
-    },    { 
-        number: 1, 
-        userImage: 'https://media-cgk1-1.cdn.whatsapp.net/v/t61.24694-24/389963334_729035019060433_5252761273192776454_n.jpg?ccb=11-4&oh=01_AdTZqJwthCfEZfOWWpFhDQPRU9L9yUiHslUaBda-M7P6tg&oe=65A224C8&_nc_sid=e6ed6c&_nc_cat=108', 
-        username: 'izumiinyaa', 
-        instagramUsername: 'qtnessdlb', 
-        description: 'Startend - Hololive'
-    },
+    }
 ];
--- script/main.js
+++ script/main.js
@@ -1,36 +1,27 @@
-// Remove .html extension from the URL
 if (window.location.pathname.endsWith('.html')) {
     var newPath = window.location.pathname.slice(0, -5);
     history.replaceState({}, document.title, newPath);
 }
 
-// Check if there's a hash in the URL
 if (window.location.hash) {
-    // If there's a hash, set the default hash to an empty string
     history.replaceState({}, document.title, window.location.pathname + window.location.search);
 }
 
-// Check if there's no hash in the URL
 if (!window.location.hash) {
-    // Set the default hash to #home
+
     history.replaceState({}, document.title, window.location.pathname + window.location.search + '#home');
 }
 
-// Handle button click to change the page without modifying the URL
 document.addEventListener('click', function (event) {
-    var target = event.target.closest('a'); // Find the nearest ancestor A element
+    var target = event.target.closest('a');
 
-    // Check if the clicked element is a navigation link
     if (target && target.getAttribute('href').startsWith('#')) {
-        event.preventDefault(); // Prevent default link behavior
+        event.preventDefault();
 
-        // Get the target section id (e.g., #home)
         var sectionId = target.getAttribute('href');
 
-        // Change the page without modifying the URL
         window.location.hash = sectionId;
 
-        // Scroll to the target section after a short delay
         setTimeout(function () {
             document.querySelector(sectionId).scrollIntoView({
                 behavior: 'smooth'
@@ -42,7 +33,7 @@ document.addEventListener('click', function (event) {
 
 // table and popup
 document.addEventListener('DOMContentLoaded', function () {
-    // Function to create table row HTML
+
     function createTableRow(user) {
         return `
             <tr>
@@ -55,14 +46,11 @@ document.addEventListener('DOMContentLoaded', function () {
         `;
     }
 
-    // Function to populate the leaderboard table
     function populateLeaderboard(data) {
         var tableBody = document.getElementById('leaderboardTable').getElementsByTagName('tbody')[0];
 
-        // Clear existing rows
         tableBody.innerHTML = '';
 
-        // Loop through the leaderboard data and create table rows
         data.forEach(function (user) {
             tableBody.insertAdjacentHTML('beforeend', createTableRow(user));
         });
