bc489d08bc9a3b57dd879a0b3297ed928301ad2a
Izumiinyaa <adimasbbayuarto@gmail.com>
2024-01-13 17:35:51 +0700

    fix

--- script/downloaddb.js
+++ script/downloaddb.js
@@ -1,5 +1,11 @@
 var downloadData = [
     {
+        title: 'Terakomari Gandesblood - Pack 1',
+        description: 'Hikikomari Kyuuketsuki',
+        size: '4MB - By Reiyu インドラ',
+        link: './download/animepict9-ctn0-Terakomari_Gandesblood.zip'
+    },
+    {
         title: 'Ellen Joe',
         description: 'Zenless Zone Zero',
         size: '58.7MB - By Admin',
--- script/leaderboard.js
+++ script/leaderboard.js
@@ -5,12 +5,14 @@ var leaderboardData = [
         username: 'harutoみずち', 
         instagramUsername: 'tsukumiharuto', 
         description: 'Phase 1 - Pack 0 / 2'
+        // IndraOne_
     },
     { 
         number: 2, 
         userImage: 'https://media-cgk1-1.cdn.whatsapp.net/v/t61.24694-24/405183837_330799403017499_3669056780112851839_n.jpg?ccb=11-4&oh=01_AdT6cUwdNlpk47XtCq5wYo2UFvOB1iE5rBRKmxULXeStbw&oe=65AD9AB2&_nc_sid=e6ed6c&_nc_cat=101', 
         username: 'Reiyu インドラ', 
         instagramUsername: 'lelouch_official77', 
-        description: 'Phase 1 - Pack 0 / 2'
+        description: 'Phase 1 - Pack 1 / 2'
+        // epan / tsukumiharuto
     }
 ];
