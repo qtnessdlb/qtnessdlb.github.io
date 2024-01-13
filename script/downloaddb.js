2fafa4aa677216253849117bb1f05573b162ea1c
Izumiinyaa <adimasbbayuarto@gmail.com>
2024-01-13 17:52:14 +0700

    upd

--- script/downloaddb.js
+++ script/downloaddb.js
@@ -1,5 +1,11 @@
 var downloadData = [
     {
+        title: 'Startend - Pack 1',
+        description: 'Hololive',
+        size: '2.5MB - By Reiyu インドラ',
+        link: './download/animepict9-ctn0-Startend_Hololive.zip'
+    },
+    {
         title: 'Terakomari Gandesblood - Pack 1',
         description: 'Hikikomari Kyuuketsuki',
         size: '4MB - By Reiyu インドラ',
@@ -10,5 +16,5 @@ var downloadData = [
         description: 'Zenless Zone Zero',
         size: '58.7MB - By Admin',
         link: './download/animepict9-ctn0-Ellen_Joe-Zenless_Zone_Zero.rar'
-    },
+    }
 ];
