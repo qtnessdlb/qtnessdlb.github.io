64eb04b281740796c1ec6fc04f19aa14cb9389c4
Izumiinyaa <adimasbbayuarto@gmail.com>
2024-01-13 16:45:25 +0700

    change : update the list

--- script/leaderboard.js
+++ script/leaderboard.js
@@ -1,9 +1,16 @@
 var leaderboardData = [
     { 
         number: 1, 
-        userImage: '', 
-        username: 'izumiinyaa', 
-        instagramUsername: 'qtnessdlb', 
-        description: 'Startend - Hololive'
+        userImage: 'https://media-cgk1-1.cdn.whatsapp.net/v/t61.24694-24/416485838_387379110416626_1468422329892190602_n.jpg?ccb=11-4&oh=01_AdSTA_osdqeDIlogjjpVIpSXNNKi_qyggk5pOnEcL3v_pg&oe=65ADE898&_nc_sid=e6ed6c&_nc_cat=101', 
+        username: 'harutoみずち', 
+        instagramUsername: 'tsukumiharuto', 
+        description: 'Phase 1 - Pack 0 / 2'
+    },
+    { 
+        number: 2, 
+        userImage: 'https://media-cgk1-1.cdn.whatsapp.net/v/t61.24694-24/405183837_330799403017499_3669056780112851839_n.jpg?ccb=11-4&oh=01_AdT6cUwdNlpk47XtCq5wYo2UFvOB1iE5rBRKmxULXeStbw&oe=65AD9AB2&_nc_sid=e6ed6c&_nc_cat=101', 
+        username: 'Reiyu インドラ', 
+        instagramUsername: 'lelouch_official77', 
+        description: 'Phase 1 - Pack 0 / 2'
     }
 ];
