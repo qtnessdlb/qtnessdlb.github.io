68b90817dc8bc2802672c6ce8d284f9460e3108d
izumiinyaa <adimasbbayuarto@email.com>
2024-01-13 19:17:59 +0700

    fix

--- script/downloaddb.js
+++ script/downloaddb.js
@@ -1,20 +1,20 @@
 var downloadData = [
     {
-        title: 'Startend - Pack 1',
+        title: 'WP Startend - Pack 1',
         description: 'Hololive',
         author: 'By Reiyu インドラ',
         size: '2.5MB',
         link: './download/animepict9-ctn0-Startend_Hololive.zip'
     },
     {
-        title: 'Terakomari - Pack 1',
+        title: 'WP Terakomari - Pack 1',
         description: 'Hikikomari Kyuuketsuki',
         author: 'By Reiyu インドラ',
         size: '4MB',
         link: './download/animepict9-ctn0-Terakomari_Gandesblood.zip'
     },
     {
-        title: 'Ellen Joe',
+        title: 'WP Ellen Joe - Pack 1',
         description: 'Zenless Zone Zero',
         author: 'By Admin',
         size: '58.7MB',
