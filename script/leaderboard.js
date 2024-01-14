9c104d12c99e4381e1168554a1d9fd1b55b553db
izumiinyaa <adimasbbayuarto@email.com>
2024-01-14 19:48:49 +0700

    update : user update

--- script/leaderboard.js
+++ script/leaderboard.js
@@ -1,6 +1,6 @@
 var leaderboardData = [
     { 
-        number: 1, 
+        number: 150, 
         userImage: 'https://media-cgk1-1.cdn.whatsapp.net/v/t61.24694-24/405183837_330799403017499_3669056780112851839_n.jpg?ccb=11-4&oh=01_AdT6cUwdNlpk47XtCq5wYo2UFvOB1iE5rBRKmxULXeStbw&oe=65AD9AB2&_nc_sid=e6ed6c&_nc_cat=101', 
         username: 'Reiyu インドラ', 
         instagramUsername: 'lelouch_official77', 
@@ -8,18 +8,18 @@ var leaderboardData = [
         // IndraOne_
     },
     { 
-        number: 2, 
+        number: 65, 
         userImage: 'https://media-cgk1-1.cdn.whatsapp.net/v/t61.24694-24/416485838_387379110416626_1468422329892190602_n.jpg?ccb=11-4&oh=01_AdSTA_osdqeDIlogjjpVIpSXNNKi_qyggk5pOnEcL3v_pg&oe=65ADE898&_nc_sid=e6ed6c&_nc_cat=101', 
         username: 'harutoみずち', 
         instagramUsername: 'tsukumiharuto', 
         description: 'Phase 1 - Pack 1 / 2'
         // epan / tsukumiharuto
     },
-        { 
-        number: 3, 
+    { 
+        number: 0, 
         userImage: 'https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg', 
         username: 'D.Ace', 
-        instagramUsername: 'jnl.dalfnn_', 
+        instagramUsername: 'jnl.dalfnnn_', 
         description: 'Phase 1 - pack 0 / 2'
         // dlfnn
     },
