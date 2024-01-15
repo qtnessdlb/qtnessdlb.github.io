760b5818f8fa08d35e1464e600d190621690c4df
Izumiinyaa <adimasbbayuarto@gmail.com>
2024-01-15 07:57:11 +0700

    fix : all update

--- script/downloaddb.js
+++ script/downloaddb.js
@@ -1,23 +1,30 @@
 var downloadData = [
     {
+        title: 'WP Terakomari - Pack 2',
+        description: 'Hikikomari Kyuuketsuki',
+        author: 'By harutoみずち',
+        size: '3.51MB',
+        link: './download/animepict9-ctn0-Terakomari_Gandesblood-pack2.zip'
+    },
+    {
         title: 'WP Startend - Pack 1',
         description: 'Hololive',
         author: 'By Reiyu インドラ',
         size: '2.5MB',
-        link: './download/animepict9-ctn0-Startend_Hololive.zip'
+        link: './download/animepict9-ctn0-Startend_Hololive-pack1.zip'
     },
     {
         title: 'WP Terakomari - Pack 1',
         description: 'Hikikomari Kyuuketsuki',
         author: 'By Reiyu インドラ',
         size: '4MB',
-        link: './download/animepict9-ctn0-Terakomari_Gandesblood.zip'
+        link: './download/animepict9-ctn0-Terakomari_Gandesblood-pack1.zip'
     },
     {
         title: 'WP Ellen Joe - Pack 1',
         description: 'Zenless Zone Zero',
         author: 'By Admin',
         size: '58.7MB',
-        link: './download/animepict9-ctn0-Ellen_Joe-Zenless_Zone_Zero.rar'
+        link: './download/animepict9-ctn0-Ellen_Joe-Zenless_Zone_Zero-pack1.rar'
     }
 ];
