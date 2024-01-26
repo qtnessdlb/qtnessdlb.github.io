aaa4b8dbf3497cf878c1407f74328e018af5d991
Izumiinyaa <adimasbbayuarto@gmail.com>
2024-01-26 09:45:30 +0700

    update : static leaderboard page fix : some css issue on mobile

--- assets/script/main.js
+++ assets/script/main.js
@@ -90,6 +90,44 @@ $(document).ready(function() {
 
 // sound handler
 
+// cdEvent handler
+var cdEvent;
+
+var compareDate = new Date();
+compareDate.setDate(compareDate.getDate() + 2);
+
+cdEvent = setInterval(function() {
+  timeBetweenDates(compareDate);
+}, 1000);
+
+function timeBetweenDates(toDate) {
+  var dateEntered = toDate;
+  var now = new Date();
+  var difference = dateEntered.getTime() - now.getTime();
+
+  if (difference <= 0) {
+
+    // cdEvent done
+    clearInterval(cdEvent);
+  
+  } else {
+    
+    var seconds = Math.floor(difference / 1000);
+    var minutes = Math.floor(seconds / 60);
+    var hours = Math.floor(minutes / 60);
+    var days = Math.floor(hours / 24);
+
+    hours %= 24;
+    minutes %= 60;
+    seconds %= 60;
+
+    $("#days").text(days);
+    $("#hours").text(hours);
+    $("#minutes").text(minutes);
+    $("#seconds").text(seconds);
+  }
+}
+
 
 
 // mobile
