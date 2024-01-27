f0dc6aa7a6512f05bc7da6e8e83e8af10facd731
izumiinyaa <adimasbbayuarto@email.com>
2024-01-28 00:37:33 +0700

    kkkk

--- assets/script/dataleaderboard.js
+++ assets/script/dataleaderboard.js
@@ -6,9 +6,9 @@ var users = [
         socialLink: "instagram.com",
         imageUrl: "https://media-cgk1-2.cdn.whatsapp.net/v/t61.24694-24/379811005_395624739707709_6210896889879609901_n.jpg?ccb=11-4&oh=01_AdQjfvwNaRLFfwPtJe6AQr7noc6Nh-B_OrngKY3iBI1rJA&oe=65BF679C&_nc_sid=e6ed6c&_nc_cat=105",
         points: 460,
-        completedTasks: 3,
-        goingTasks: 3,
-        phaseProgress: "Completed Phase 2"
+        completedTasks: 0,
+        goingTasks: 4,
+        phaseProgress: "Phase 3"
     },
     {
         name: "Reiyu インドラ",
@@ -16,9 +16,9 @@ var users = [
         socialLink: "instagram.com",
         imageUrl: "https://media-cgk1-1.cdn.whatsapp.net/v/t61.24694-24/405183837_330799403017499_3669056780112851839_n.jpg?ccb=11-4&oh=01_AdReLukrml3xzwaJ4vHQP4m4CvLGoQsENyLBDZnoUWpg-Q&oe=65BDDD32&_nc_sid=e6ed6c&_nc_cat=101",
         points: 450,
-        completedTasks: 3,
-        goingTasks: 3,
-        phaseProgress: "Completed Phase 2"
+        completedTasks: 0,
+        goingTasks: 4,
+        phaseProgress: "Phase 3"
     },
     {
         name: "izha",
@@ -27,8 +27,8 @@ var users = [
         imageUrl: "https://media-cgk1-1.cdn.whatsapp.net/v/t61.24694-24/328794901_1257311118482826_199441960752094635_n.jpg?ccb=11-4&oh=01_AdSTNfDLLyb_bJwn9AOvJYH_sYfjGGG-lKcGiGSmzLT2rA&oe=65BE11D3&_nc_sid=e6ed6c&_nc_cat=107",
         points: 150,
         completedTasks: 0,
-        goingTasks: 3,
-        phaseProgress: "Phase 2"
+        goingTasks: 7,
+        phaseProgress: "Phase 2 & 3"
     },
     {
         name: "zainudin",
@@ -37,8 +37,8 @@ var users = [
         imageUrl: "https://media-cgk1-1.cdn.whatsapp.net/v/t61.24694-24/353111909_947959676319175_6662605930404281510_n.jpg?ccb=11-4&oh=01_AdQYc_FQrYGJVhS-4prnQqpT5S2XLfLSFqo7FVjxoJLTrQ&oe=65BE09DF&_nc_sid=e6ed6c&_nc_cat=103",
         points: 0,
         completedTasks: 0,
-        goingTasks: 5,
-        phaseProgress: "Phase 1 & 2"
+        goingTasks: 9,
+        phaseProgress: "Phase 1 - 3"
     },
     {
         name: "D.Ace",
@@ -47,8 +47,8 @@ var users = [
         imageUrl: "https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg",
         points: 0,
         completedTasks: 0,
-        goingTasks: 5,
-        phaseProgress: "Phase 1 & 2"
+        goingTasks: 9,
+        phaseProgress: "Phase 1 - 3"
     },
     
 ];
