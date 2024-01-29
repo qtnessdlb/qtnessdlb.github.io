b99c35865cf9fe7d21d52925cb4869ba728219b9
Izumiinyaa <adimasbbayuarto@gmail.com>
2024-01-29 17:40:36 +0700

    update : new download

--- assets/script/datadownload.js
+++ assets/script/datadownload.js
@@ -1,10 +1,19 @@
 var downloadData = [
     {
         ispremium: 'true',
+        title: 'WP Small Raphtalia - Pack 1',
+        description: 'Tate No Yuusha',
+        author: 'By Admin',
+        source: 'Pixiv',
+        size: '31 MB',
+        link: '/downloads/animepict9-ctn0-raphtalia-tate_no_yuusha-pack1.zip'
+    },
+    {
+        ispremium: 'true',
         title: 'WP Hololive - Pack 1',
         description: 'Hololive',
         author: 'By Admin',
-        source: 'Pixiv @かぃり',
+        source: 'Pixiv',
         size: '35 MB',
         link: '/downloads/animepict9-ctn0-Hololive_by_かぃり-pack1.zip'
     },
@@ -13,7 +22,7 @@ var downloadData = [
         title: 'WP Kazama Iroha - Pack 2',
         description: 'Hololive',
         author: 'By harutoみずち',
-        source: 'Whatsapp @Member',
+        source: 'Whatsapp',
         size: '3 MB',
         link: '/downloads/animepict9-ctn0-Kazama_Iroha-Hololive-pack2.zip'
     },
@@ -22,7 +31,7 @@ var downloadData = [
         title: 'WP March7 - Pack 2',
         description: 'Honkai: Star Rail',
         author: 'By harutoみずち',
-        source: 'Whatsapp @Member',
+        source: 'Whatsapp',
         size: '2 MB',
         link: '/downloads/animepict9-ctn0-March7-Starrail-pack2.zip'
     },
@@ -31,7 +40,7 @@ var downloadData = [
         title: 'WP Asada Shino - Pack 2',
         description: 'Sword Art Online',
         author: 'By harutoみずち',
-        source: 'Whatsapp @Member',
+        source: 'Whatsapp',
         size: '2 MB',
         link: '/downloads/animepict9-ctn0-Asada_Shino-SAO-pack2.zip'
     },
@@ -40,7 +49,7 @@ var downloadData = [
         title: 'WP Kazama Iroha - Pack 1',
         description: 'Hololive',
         author: 'By Reiyu インドラ',
-        source: 'Whatsapp @Member',
+        source: 'Whatsapp',
         size: '1.7 MB',
         link: '/downloads/animepict9-ctn0-Kazama_Iroha-Hololive-pack1.zip'
     },
@@ -49,7 +58,7 @@ var downloadData = [
         title: 'WP March7 - Pack 1',
         description: 'Honkai: Star Rail',
         author: 'By Reiyu インドラ',
-        source: 'Whatsapp @Member',
+        source: 'Whatsapp',
         size: '2 MB',
         link: '/downloads/animepict9-ctn0-March7-Starrail-pack1.zip'
     },
@@ -58,7 +67,7 @@ var downloadData = [
         title: 'WP Asada Shino - Pack 1',
         description: 'Sword Art Online',
         author: 'By Reiyu インドラ',
-        source: 'Whatsapp @Member',
+        source: 'Whatsapp',
         size: '2 MB',
         link: '/downloads/animepict9-ctn0-Asada_Shino-SAO-pack1.zip'
     },
@@ -67,7 +76,7 @@ var downloadData = [
         title: 'WP Startend - Pack 3',
         description: 'Hololive',
         author: 'By izha',
-        source: 'Whatsapp @Member',
+        source: 'Whatsapp',
         size: '14 MB',
         link: '/downloads/animepict9-ctn0-Startend_Hololive-pack3.zip'
     },
@@ -76,7 +85,7 @@ var downloadData = [
         title: 'WP Terakomari - Pack 3',
         description: 'Hikikomari Kyuuketsuki',
         author: 'By izha',
-        source: 'Whatsapp @Member',
+        source: 'Whatsapp',
         size: '9.8 MB',
         link: '/downloads/animepict9-ctn0-Terakomari_Gandesblood-pack3.zip'
     },
@@ -85,7 +94,7 @@ var downloadData = [
         title: 'WP Nakiri Ayame - Pack 1',
         description: 'Hololive',
         author: 'By Admin',
-        source: 'Whatsapp @Member',
+        source: 'Whatsapp',
         size: '55.5 MB',
         link: '/downloads/animepict9-ctn0-Nakiri_Ayame-Hololive-pack1.zip'
     },
@@ -94,7 +103,7 @@ var downloadData = [
         title: 'WP Startend - Pack 2',
         description: 'Hololive',
         author: 'By harutoみずち',
-        source: 'Whatsapp @Member',
+        source: 'Whatsapp',
         size: '2.8 MB',
         link: '/downloads/animepict9-ctn0-Startend_Hololive-pack2.zip'
     },
@@ -103,7 +112,7 @@ var downloadData = [
         title: 'WP Shiina Mahiru - Pack 1',
         description: 'Otonari no Tenshi',
         author: 'By Admin',
-        source: 'Whatsapp @Member',
+        source: 'Whatsapp',
         size: '14 MB',
         link: '/downloads/animepict9-src=pxv-file0-ctn=shiinamahiru-81hn92y.zip'
     },
@@ -112,7 +121,7 @@ var downloadData = [
         title: 'WP Terakomari - Pack 2',
         description: 'Hikikomari Kyuuketsuki',
         author: 'By harutoみずち',
-        source: 'Whatsapp @Member',
+        source: 'Whatsapp',
         size: '3.51 MB',
         link: '/downloads/animepict9-ctn0-Terakomari_Gandesblood-pack2.zip'
     },
@@ -121,7 +130,7 @@ var downloadData = [
         title: 'WP Startend - Pack 1',
         description: 'Hololive',
         author: 'By Reiyu インドラ',
-        source: 'Whatsapp @Member',
+        source: 'Whatsapp',
         size: '2.5 MB',
         link: '/downloads/animepict9-ctn0-Startend_Hololive-pack1.zip'
     },
@@ -130,7 +139,7 @@ var downloadData = [
         title: 'WP Terakomari - Pack 1',
         description: 'Hikikomari Kyuuketsuki',
         author: 'By Reiyu インドラ',
-        source: 'Whatsapp @Member',
+        source: 'Whatsapp',
         size: '4 MB',
         link: '/downloads/animepict9-ctn0-Terakomari_Gandesblood-pack1.zip'
     },
@@ -139,7 +148,7 @@ var downloadData = [
         title: 'WP Ellen Joe - Pack 1',
         description: 'Zenless Zone Zero',
         author: 'By Admin',
-        source: 'Whatsapp @Member',
+        source: 'Whatsapp',
         size: '58.7 MB',
         link: '/downloads/animepict9-ctn0-Ellen_Joe-Zenless_Zone_Zero-pack1.rar'
     }
