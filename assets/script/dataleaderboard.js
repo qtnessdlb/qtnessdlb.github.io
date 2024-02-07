efec538557de563ec1a2bd51d49404997a8c1d42
Izumiinyaa <adimasbbayuarto@gmail.com>
2024-02-07 09:01:31 +0700

    fix : all final data

--- assets/script/dataleaderboard.js
+++ assets/script/dataleaderboard.js
@@ -4,51 +4,20 @@ var users = [
         name: "harutoみずち",
         username: "epangotama",
         socialLink: "instagram.com",
-        imageUrl: "https://media-cgk1-2.cdn.whatsapp.net/v/t61.24694-24/379811005_395624739707709_6210896889879609901_n.jpg?ccb=11-4&oh=01_AdQjfvwNaRLFfwPtJe6AQr7noc6Nh-B_OrngKY3iBI1rJA&oe=65BF679C&_nc_sid=e6ed6c&_nc_cat=105",
+        imageUrl: "https://media-cgk1-1.cdn.whatsapp.net/v/t61.24694-24/379811005_395624739707709_6210896889879609901_n.jpg?ccb=11-4&oh=01_AdRbnOcI39txO-UC7TLqHWsEJggSEFQqtV2oQaHPzShffQ&oe=65CE90DC&_nc_sid=e6ed6c&_nc_cat=105",
         points: "Boosted - 850",
-        completedTasks: 4,
-        goingTasks: 4,
-        phaseProgress: "Phase 3 Completed"
+        completedTasks: 0,
+        goingTasks: 1,
+        phaseProgress: "Final"
     },
     {
         name: "Reiyu インドラ",
         username: "lelouch_official77",
         socialLink: "instagram.com",
-        imageUrl: "https://media-cgk1-1.cdn.whatsapp.net/v/t61.24694-24/405183837_330799403017499_3669056780112851839_n.jpg?ccb=11-4&oh=01_AdReLukrml3xzwaJ4vHQP4m4CvLGoQsENyLBDZnoUWpg-Q&oe=65BDDD32&_nc_sid=e6ed6c&_nc_cat=101",
+        imageUrl: "https://media-cgk1-1.cdn.whatsapp.net/v/t61.24694-24/405183837_330799403017499_3669056780112851839_n.jpg?ccb=11-4&oh=01_AdR3vo9pkOQUld7Tg0ttvhoYBHAvF594H7Yyae4Jws_eTw&oe=65CE9032&_nc_sid=e6ed6c&_nc_cat=101",
         points: "Boosted - 820",
-        completedTasks: 4,
-        goingTasks: 4,
-        phaseProgress: "Phase 3 Completed"
-    },
-    {
-        name: "izha",
-        username: "muhammadizhafathurahman",
-        socialLink: "instagram.com",
-        imageUrl: "https://media-cgk1-1.cdn.whatsapp.net/v/t61.24694-24/328794901_1257311118482826_199441960752094635_n.jpg?ccb=11-4&oh=01_AdSTNfDLLyb_bJwn9AOvJYH_sYfjGGG-lKcGiGSmzLT2rA&oe=65BE11D3&_nc_sid=e6ed6c&_nc_cat=107",
-        points: 150,
         completedTasks: 0,
-        goingTasks: 7,
-        phaseProgress: "Phase 2 & 3"
-    },
-    {
-        name: "zainudin",
-        username: "zainudin_flytre",
-        socialLink: "instagram.com",
-        imageUrl: "https://media-cgk1-1.cdn.whatsapp.net/v/t61.24694-24/353111909_947959676319175_6662605930404281510_n.jpg?ccb=11-4&oh=01_AdQYc_FQrYGJVhS-4prnQqpT5S2XLfLSFqo7FVjxoJLTrQ&oe=65BE09DF&_nc_sid=e6ed6c&_nc_cat=103",
-        points: 0,
-        completedTasks: 0,
-        goingTasks: 9,
-        phaseProgress: "Phase 1 - 3"
-    },
-    {
-        name: "D.Ace",
-        username: "jnl.dalfnnn_",
-        socialLink: "instagram.com",
-        imageUrl: "https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg",
-        points: 0,
-        completedTasks: 0,
-        goingTasks: 9,
-        phaseProgress: "Phase 1 - 3"
-    },
-    
+        goingTasks: 1,
+        phaseProgress: "Final"
+    }, 
 ];
